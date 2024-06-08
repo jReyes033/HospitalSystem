a1ecbc1c2fb52e1dd1a434e47d3cb1ba1af39dff
jReyes033 <reyesjustine033@gmail.com>
2024-06-08 15:11:18 +0800

    Recreation of project

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('user', [UserController::class, 'index']);
+Route::post('user', [UserController::class,'store']);
+Route::get('user/{id}', [UserController::class,'show']);
+Route::get('user/{id}/edit', [UserController::class,'edit']);
+Route::put('user/{id}/edit', [UserController::class,'update']);
+Route::delete('user/{id}/delete', [UserController::class,'destroy']);
